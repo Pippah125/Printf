126ec0222df3d6bfab8cbe0c47b7c13f53fb8df1
Pippah125 <Pippah.gift@gmail.com>
2023-10-17 19:41:04 +0000

    alx

---
+++ callback.c
@@ -0,0 +1,22 @@
+#include"main.h"
+int callback(int num)
+{
+	int count = 0;
+	if(num == 0)
+	{
+		return (0);
+	}
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		count ++;
+											}
+	if ( num >= 10)
+	{
+		count += callback(num /10);
+	}
+	_putchar((num % 10)+ '0');
+	count++;
+	return(count);
+}
---
+++ printf.c
@@ -0,0 +1,55 @@
+#include"main.h"
+#include<stdarg.h>
+#include<stddef.h>
+int _printf(const char *format, ...)
+{
+	int x;
+	int y = 0;
+	int v;
+	va_list print;
+	va_start(print, format);
+	if(format == NULL)
+	{
+		return(-1);
+	}
+	for (x = 0; format[x] != '\0'; x++)
+	{
+		if(format[x] != '%')
+		{
+		_putchar(format[x]);
+		}
+		else if(format[x +1] == 'c')
+		{
+			_putchar(va_arg(print, int));
+			x++;									  }
+		else if(format[x +1] == 's')
+		{
+			char * betty = (va_arg(print, char *));
+		if(betty == NULL)
+		{
+			betty = "(null)";
+		}
+		
+		v = _puts(betty );
+		x++;
+		y += (v - 1);
+		}
+	else if(format[x + 1] == '%')																		{
+		_putchar('%');
+		x++;									      }
+																						else if (format[x +1] == 'd')
+	{
+		int num = (va_arg(print, int));
+		y += (callback(num));
+		x++;																					_putchar('\n');
+	}
+	else
+	{
+		_putchar(format[x]);
+																						}
+
+	y++;
+																						}
+	va_end(print);
+	return (y);
+}
